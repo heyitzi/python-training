8e222450ea80e4a60a58ad845f961f7057a48e95
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-02 12:23:42 +0100

    Solve exercise 23 and create 22

---
+++ 23_hate_vowels.py
@@ -0,0 +1,7 @@
+def vowel_remover(str):
+    for vowel in "aeiouAEIOU":
+        str = str.replace(vowel, "*")
+    return str
+
+my_str = input("Write whatever you want, and I'll remove the vowels for you: ")
+print(vowel_remover(my_str))
