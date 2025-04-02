5ef53542bb46e28a911f984e228fd5b36f1bb88b
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-02 10:40:34 +0100

    Solve exercise 05

--- 05_type_conversion.py
+++ 05_type_conversion.py
@@ -1,5 +1,11 @@
-def turntables(param):
-    pass    
+def turntables(num, type):
+    if type == 1:
+        return num
+    else:
+        return str(num)
 
 
-# Input always returns a string...
+user_num = input("Enter a number of your choice: ")
+user_type = input("Choose 1 if you want to return it as a number or 2 if you prefer a string")
+
+print(turntables(int(user_num), int(user_type)))
