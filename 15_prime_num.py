ce81e8dc99abec4a3f7bc57c07a9eeefc4c27186
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-02 11:00:16 +0100

    Solve exercise 16

---
+++ 15_prime_num.py
@@ -0,0 +1,5 @@
+def prime_num(num):
+    pass
+
+user_num = input("Enter your number to find out if it's prime: ")
+print(prime_num(int(user_num)))
---
+++ 16_movie_list.py
@@ -0,0 +1,4 @@
+fav_movies = ["Mr. Nobody", "The Fifth Element", "Frances Ha", "The Grand Hotel Budapest", "Amelie"]
+
+for movie in fav_movies:
+    print(movie)
