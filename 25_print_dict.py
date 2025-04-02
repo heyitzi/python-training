2960169c02484c27afef59030629dbba177e7efb
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-02 12:38:28 +0100

    Solve exercise 25

---
+++ 25_print_dict.py
@@ -0,0 +1,41 @@
+tog = {
+    1: "The Assassin's Blade",
+    2: "Throne of Glass",
+    3: "Crown of Midnight",
+    4: "Heir of Fire",
+    5: "Queen of Shadows",
+    6: "Empire of Storms",
+    7: "Tower of Dawn",
+    8: "Kingdom of Ash"
+}
+
+acotar = {
+    1: "A Court of Thornes and Roses",
+    2: "A Court of Myst and Fury",
+    3: "A Court of Wings and Ruin",
+    4: "A Court of Frost and Starlight",
+    5: "A Court of Silver Flames"
+}
+
+cc = {
+    1: "House of Earth and Blood",
+    2: "House of Sky and Breath",
+    3: "House of Flame and Shadow"
+}
+
+print("Sarah J. Maas sagas:")
+print("-----")
+
+print("TOG Series")
+for i in tog:
+    print(i, tog[i])
+print("-----")
+
+print("ACOTAR Series")
+for i in acotar:
+    print(i, acotar[i])
+print("-----")
+
+print("Crescent City Series")
+for i in cc:
+    print(i, cc[i])
