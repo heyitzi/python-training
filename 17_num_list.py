c458cbdcb4438eb4e31452c1342933a87bc134c3
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-02 11:12:50 +0100

    Solve exercise 17

---
+++ 17_num_list.py
@@ -0,0 +1,10 @@
+numbers = [4, 678, 2, 56, 90, 35, 76, 6839, 5]
+
+biggest_num = max(numbers)
+print(f"Max number in the list: {biggest_num}")
+
+smallest_num = min(numbers)
+print(f"Min number in the list is: {smallest_num}")
+
+average = sum(numbers) / len(numbers)
+print(f"The average is: {average}")
