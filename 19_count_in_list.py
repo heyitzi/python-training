4c8f1dd05c8b0455bf2033ec84852438ead64638
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-03 13:05:36 +0100

    Explore exerc 19 in depth

--- 19_count_in_list.py
+++ 19_count_in_list.py
@@ -1,7 +1,16 @@
 my_list = ["a", "b", "c", "a", "a", "a", "c", "b", "d", "e", "d", "c"]
 
-my_dict = {letter: my_list.count(letter) for letter in my_list}
-
-print(my_dict)
+# my_dict = {letter: my_list.count(letter) for letter in my_list}
+# print(my_dict)
 
 # TODO hacer de manera normal
+
+new_dict = {}
+
+for letter in my_list:
+    if letter in new_dict:
+        new_dict[letter] += 1
+    else:
+        new_dict[letter] = 1
+
+print(new_dict)
