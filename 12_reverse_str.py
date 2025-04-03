f2d00c73c12b2bf8f07972705571c3c88c6221ae
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-03 12:36:48 +0100

    Explore exerc 12 in depth

--- 12_reverse_str.py
+++ 12_reverse_str.py
@@ -3,3 +3,14 @@ word = input("Write and I'll reverse it: ")
 print(word[::-1])
 
 # TODO explore slice more
+
+# string[start_position:end_position:step]
+# start_position (included) / end position (excluded)
+
+s = "My name is Itz"
+s1 = s[3:12:3]
+print(s1)
+
+s2 = "AnotherString"
+s3 = s2[:1:-1]
+print(s3)
