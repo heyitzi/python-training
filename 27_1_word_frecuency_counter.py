716ea13efd143852ccea6ebd4f80b210d5a96579
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-04 15:09:50 +0100

    Created exerc 27_1 due to misunderstanding 27

---
+++ 27_1_word_frecuency_counter.py
@@ -0,0 +1,13 @@
+user_string = input("Write something, and I'll let you know the frequency of each word: ")
+split_str = user_string.lower().split()
+
+counting_dict = {}
+
+for word in split_str:
+    if word in counting_dict:
+        counting_dict[word] += 1
+        counting_dict[word] += 1
+    else:
+        counting_dict[word] = 1
+
+print(counting_dict)
