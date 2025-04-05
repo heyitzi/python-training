90a039baa2a0bf0adbde563f0dba6df402f1b83f
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-05 12:14:56 +0100

    Fix checking the username and password using not in

--- 28_login.py
+++ 28_login.py
@@ -20,26 +20,27 @@ while True:
         print("Log in:")
         username = input("Enter your username: ")
         password = input("Enter your password: ")
-        if username_list.index(username) != -1 and password_list.index(password) != -1:
-            print("----------")
-            print("Welcome! You have successfully logged in")
-            print("----------")
-        else:
+        if username not in username_list and password not in password_list:
             print("----------")
             print("Sorry, the username or password you have entered is incorrect")
             print("Please try again")
             print("----------")
+        else:
+            print("----------")
+            print("Welcome! You have successfully logged in")
+            print("----------")
     elif option == 2:
         print("Create a new account:")
         username = input("Enter your username: ")
-        password = input("Enter your password: ")
-        if username_list.index(username) != -1:
+        if username in username_list:
             print("----------")
             print("Sorry, the username already exists")
             print("Please try again")
             print("----------")
         else:
-            print("----------")
+            password = input("Enter your password: ")
+            username_list.append(username)
+            password_list.append(password)
             print("Yay! You have successfully created your account")
             print("You can now log in")
             print("----------")
