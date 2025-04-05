e68cdb0339a7e6b03917dd1afe09c1f4f200261c
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-05 17:06:16 +0100

    Delete dupicated line

--- 19_count_in_list.py
+++ 19_count_in_list.py
@@ -3,7 +3,6 @@ my_list = ["a", "b", "c", "a", "a", "a", "c", "b", "d", "e", "d", "c"]
 # my_dict = {letter: my_list.count(letter) for letter in my_list}
 # print(my_dict)
 
-# TODO hacer de manera normal
 
 new_dict = {}
 
