6e54fef1d1d0ec35882d1293e3ff2838962b72d4
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-05 11:42:33 +0100

    Fix exercise 15

--- 15_prime_num.py
+++ 15_prime_num.py
@@ -1,14 +1,15 @@
 def prime_num(num):
-    if num > 1:
-        for n in range(2, num - 1):
+    if num == 2:
+        return f"{num} is a prime number"
+    elif num > 2:
+        for n in range(2, num):
             if num % n == 0:
                 return f"{num} is not a prime number"
-            else:
-                return f"{num} is a prime number"
+        return f"{num} is a prime number"
+    elif num <= 0:
+        return "Only positive numbers can be prime, please enter a valid option."
     else:
         return "I know, I know, 1 should be a prime number... but it's not!"
 
 user_num = input("Enter a positive integer to find out if it's prime: ")
 print(prime_num(int(user_num)))
-
-# TODO FIX 
