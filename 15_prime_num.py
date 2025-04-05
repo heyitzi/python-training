cc7b24fd361ca5938b03bff56ee858c41fa21035
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-05 17:05:52 +0100

    Refactor code

--- 15_prime_num.py
+++ 15_prime_num.py
@@ -1,7 +1,5 @@
 def prime_num(num):
-    if num == 2:
-        return f"{num} is a prime number"
-    elif num > 2:
+    if num >= 2:
         for n in range(2, num):
             if num % n == 0:
                 return f"{num} is not a prime number"
