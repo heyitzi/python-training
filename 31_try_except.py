db46fefe9ad387b76265520754549827d157e25b
Itziar Cantero <itziar.cantero26@gmail.com>
2025-04-08 12:51:37 +0100

    Remove todo

--- 31_try_except.py
+++ 31_try_except.py
@@ -1,5 +1,5 @@
 try:
-    user_input = int(input("Enter a number: "))
+    user_input = float(input("Enter a number: "))
     print(f"Yay! {user_input} is a number")
 except ValueError:
     print("Well, that is not a number, isn't it?")
